214074f91012ddd010ff13171e39badeda817639
Remigijus Klimovas <remigijus.klimovas@vinted.com>
2022-04-19 16:24:07 +0300

    trackForMemoryLeaks

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -93,11 +93,29 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
 
+        trackForMemoryLeaks(client)
+        trackForMemoryLeaks(sut)
+
         return (sut, client)
+    }
+
+    private func trackForMemoryLeaks(_ instance: AnyObject,
+                                     file: StaticString = #filePath,
+                                     line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(
+                instance,
+                "Instance should have been dealocated. Potential memory leak.",
+                file: file,
+                line: line
+            )
+        }
     }
 
     private func makeItem(id: UUID,

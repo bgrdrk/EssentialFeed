93e4019a751fbfbe30e1fbe6c74772d3f63f670f
Remigijus Klimovas <remigijus.klimovas@vinted.com>
2022-04-20 14:25:34 +0300

    update error comparison to work with iOS 14+

--- EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
@@ -31,7 +31,8 @@ class URLSessionHTTPCientTests: XCTestCase {
         sut.get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
-                XCTAssertEqual(receivedError, error)
+                XCTAssertEqual(receivedError.domain, error.domain)
+                XCTAssertEqual(receivedError.code, error.code)
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instead")
             }

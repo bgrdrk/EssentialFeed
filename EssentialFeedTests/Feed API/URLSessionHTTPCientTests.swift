1b295d823d9ba20125bdcd283fded75e2b3da279
Remigijus Klimovas <remigijus.klimovas@vinted.com>
2022-04-20 13:16:16 +0300

    Delivers failure with error on request error

--- EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import EssentialFeed
 
 class URLSessionHTTPClient {
     private let session: URLSession
@@ -7,8 +8,12 @@ class URLSessionHTTPClient {
         self.session = session
     }
 
-    func get(from url: URL) {
-        session.dataTask(with: url, completionHandler: { _, _, _ in }).resume()
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url, completionHandler: { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }).resume()
     }
 }
 
@@ -23,23 +28,58 @@ class URLSessionHTTPCientTests: XCTestCase {
 
         let sut = URLSessionHTTPClient(session: session)
 
-        sut.get(from: url)
+        sut.get(from: url) { _ in }
 
         XCTAssertEqual(task.resumeCallCount, 1)
+    }
+
+    func test_getFromURL_failsOnRequestError() {
+        let url = URL(string: "https://any-url.com")!
+        let error = NSError(domain: "any error", code: 1)
+        let session = URLSessionSpy()
+        session.stub(url: url, error: error)
+        let sut = URLSessionHTTPClient(session: session)
+
+        let expectation = expectation(description: "wait for completion")
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(receivedError as NSError):
+                XCTAssertEqual(receivedError, error)
+            default:
+                XCTFail("Expected failure with error \(error), got \(result) instead")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1.0)
     }
 
     // MARK: - Helpers
 
     // Subclassing is dangerous in this case. We don't own these classes, they are foundation classes and we down't have access to te implementations. These contains bunch of methods we don't use, etc.
     private class URLSessionSpy: URLSession {
-        private var stubs = [URL: URLSessionDataTask]()
+        private var stubs = [URL: Stub]()
 
-        func stub(url: URL, task: URLSessionDataTask) {
-            stubs[url] = task
+        private struct Stub {
+            let task: URLSessionDataTask
+            let error: Error?
         }
 
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            stubs[url] ?? FakeURLSessionDataTask()
+        func stub(url: URL,
+                  task: URLSessionDataTask = FakeURLSessionDataTask(),
+                  error: Error? = nil) {
+            stubs[url] = .init(task: task, error: error)
+        }
+
+        override func dataTask(with url: URL,
+                               completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            guard let stub = stubs[url] else {
+                fatalError("Couldn't find stub for \(url)")
+            }
+
+            completionHandler(nil, nil, stub.error)
+            return stub.task
         }
     }
 

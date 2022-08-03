06e30c6f69ee493cbf44ee5f98f58be778165d0d
Remigijus Klimovas <remigijus@klimovas.lt>
2022-08-03 09:34:30 +0300

    Replace custom HTTPClientResult enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPCientTests.swift
@@ -111,12 +111,12 @@ class URLSessionHTTPCientTests: XCTestCase {
                            response: URLResponse?,
                            error: Error?,
                            file: StaticString = #filePath,
-                           line: UInt = #line) -> HTTPClientResult {
+                           line: UInt = #line) -> HTTPClient.Result {
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         let expectation = expectation(description: "wait for completion")
 
-        var receivedResult: HTTPClientResult!
+        var receivedResult: HTTPClient.Result!
         sut.get(from: anyURL) { result in
             receivedResult = result
             expectation.fulfill()

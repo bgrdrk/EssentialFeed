7bdd3e614fdf1da5879ab108b8aa725ec86bb931
Remigijus Klimovas <remigijus@klimovas.lt>
2022-10-06 12:36:53 +0300

    Extract table view configuration into a helper method

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -19,9 +19,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = dataSource
-        dataSource.defaultRowAnimation = .right
-        configureErrorView()
+        configureTableView()
         refresh()
     }
     
@@ -63,20 +61,10 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         }
     }
     
-    private func configureErrorView() {
-        let container = UIView()
-        container.backgroundColor = .clear
-        container.addSubview(errorView)
-        
-        errorView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            container.trailingAnchor.constraint(equalTo: errorView.trailingAnchor),
-            errorView.topAnchor.constraint(equalTo: container.topAnchor),
-            container.bottomAnchor.constraint(equalTo: errorView.bottomAnchor),
-        ])
-        
-        tableView.tableHeaderView = container
+    private func configureTableView() {
+        dataSource.defaultRowAnimation = .right
+        tableView.dataSource = dataSource
+        tableView.tableHeaderView = errorView.makeContainer()
         
         errorView.onHide = { [weak self] in
             self?.tableView.beginUpdates()
